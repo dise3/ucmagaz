291b91bd7ebad1ec339b1e0c297012bbfaf07c77
Твоё имя <@email.com>
2026-07-25 23:53:33 +0300

    currency7

--- client/src/pages/Steam.tsx
+++ client/src/pages/Steam.tsx
@@ -252,7 +252,7 @@ const Steam: React.FC<SteamProps> = ({ onBack }) => {
               <div className="flex justify-between text-sm border-t border-white/10 pt-2">
                 <span className="text-white font-bold">Итого</span>
                 <span className="text-white font-bold">
-                  {finalPrice}
+                  {finalPrice} ₽
                 </span>
               </div>
             </div>
